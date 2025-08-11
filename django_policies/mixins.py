b4f170f78690980f59003aa8ef949abaf24f0fed
Adam Zahradník <adam@zahradnik.xyz>
2025-08-11 17:40:08 +0200

    feat: automatic object retrieval in PermissionRequiredMixin

--- django_policies/mixins.py
+++ django_policies/mixins.py
@@ -1,10 +1,19 @@
+from typing import Any
 from django.contrib.auth.mixins import (
     PermissionRequiredMixin as BasePermissionRequiredMixin,
 )
+from django.views.generic.edit import BaseCreateView
 
 
 class PermissionRequiredMixin(BasePermissionRequiredMixin):
-    def get_permission_object(self):
+    def get_permission_object(self) -> Any:
+        if hasattr(self, "object"):
+            return getattr(self, "object")
+
+        if not isinstance(self, BaseCreateView):
+            if hasattr(self, "get_object"):
+                return getattr(self, "get_object")()
+
         return None
 
     def has_permission(self) -> bool:
